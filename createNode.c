94b6d16573f755a52dafd5a8c13e941323b721fd
Yash Joshi <43046108+ThePhilomathsApprentice@users.noreply.github.com>
2019-01-21 00:06:54 +0530

    File Moved to EmployeeData_LinkedList

--- createNode.c
+++
@@ -1,8 +0,0 @@
-#include "myheader.h"
-#include<stdlib.h>
-
-EMP *createNode(void){
-	EMP *node = (EMP *) malloc( sizeof(EMP)); /* Dynamic memory request of size struct employee aka EMP */
-	return node;
-}
-

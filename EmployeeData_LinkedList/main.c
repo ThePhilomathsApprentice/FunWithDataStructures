70fd9b943dc03028ff9a2b11586ec97754c9635f
ThePhilomathsApprentice <yashjoshi3995@gmail.com>
2019-07-24 23:32:59 +0530

    -Added description to the options chosen.

--- EmployeeData_LinkedList/main.c
+++ EmployeeData_LinkedList/main.c
@@ -6,12 +6,15 @@ int main(){
 	while(choice = showMenu()){
 		switch(choice){
 			case 1:
+				printf("Create Employee Chosen. Thank You. ;)");
 				createEmp(&head);
 				break;
 			case 2:
+				printf("Display Employees Chosen. Thank You. ;)");
 				display(&head);
 				break;
 			case 3:
+				printf("Display Employee with highest and lowest salary chosen. Thank You. ;)");
 				dispHighLow(&head);
 				break;
 			default:

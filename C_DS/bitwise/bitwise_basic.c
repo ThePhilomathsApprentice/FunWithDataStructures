15ebd324fe7451ffb4cfaf3326d8a2f92a0c3de4
Yash Joshi <yashjoshi3995@gmail.com>
2023-09-25 15:46:45 +0100

    minor: added more operations.

--- C_DS/bitwise/bitwise_basic.c
+++ C_DS/bitwise/bitwise_basic.c
@@ -1,18 +1,74 @@
 /* Example explaining basic bitwise operations.
  */
 #include <stdio.h>
+#include <stdint.h>
+
+/* Print 8bit number in binary.
+ */
+void print_binary(uint8_t num)
+{
+    for (int index = 7; index >= 0; index--)
+    {
+
+        if (index == 3)
+        {
+            /* put a space for legibility
+             */
+            printf(" ");
+        }
+
+        /* print the MSB first
+         */
+        printf("%s", (num & (0x1 << index)) ? "1" : "0");
+    }
+    printf(" (decimal:%d)\n", num);
+}
 
 /* Driver code
  */
 int main()
 {
 
-    int a = 1000, b = 4000;
-    int result = a & b;
-    int result_1 = a | b;
-    printf("a = %d(%x), b = %d(%x).\n", a, a, b, b);
-    printf("result = a & b = 0x%x.\n", result);
-    printf("result_1 = a | b = 0x%x.\n", result_1);
+    uint8_t a = 100, b = 255;
+    uint8_t bAND = a & b;
+    uint8_t bOR = a | b;
+    uint8_t bXOR = a ^ b;
+    uint8_t buNOT = !a;           // bitwise unsigned NOT
+    uint8_t bsNOT = ~a;           // bitwise signed NOT
+    uint8_t left_shift = b << 1;  // left Shift
+    uint8_t right_shift = a >> 1; // right Shift
+
+    printf("\n");
+
+    printf("a= ");
+    print_binary(a);
+    printf("b= ");
+    print_binary(b);
+    printf("\n");
+
+    printf("bAND = a & b = ");
+    print_binary(bAND);
+    printf("bOR  = a | b = ");
+    print_binary(bOR);
+    printf("bXOR = a ^ b = ");
+    print_binary(bXOR);
+    printf("\n");
+
+    printf("buNOT = !a = ");
+    print_binary(buNOT);
+    printf("bsNOT = ~a = ");
+    print_binary(bsNOT);
+    printf("\n");
+
+    printf("left_shift = b << 1 = ");
+    print_binary(left_shift);
+    printf("b = ");
+    print_binary(b);
+    printf("right_shift = a >> 1 = ");
+    print_binary(right_shift);
+    printf("a = ");
+    print_binary(a);
+    printf("\n");
 
     return 0;
 }

4b848de161c5ac7be896cfa9f3c70eb3bc2ad96b
Yash Joshi <yashj@juniper.net>
2025-04-20 17:05:25 +0530

    minor: trimmed white spaces

--- CPP_DS/balancedArray.cpp
+++ CPP_DS/balancedArray.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 /*
- * @return: index of the element around which array is balanced, 
+ * @return: index of the element around which array is balanced,
  *  -1: if not balanced.
  * */
 int balancedArrayIndex(vector<int> arr){
-    
+
     int left = 0;
     int right = accumulate(arr.begin(), arr.end(), 0);
-    
-    
+
+
     for( unsigned  int i =0 ; i<arr.size() ; i++ ){
-	
+
 	right -= arr[i];
-	
+
 	if( left == right){
 	    return i;
-	    
+
 	    }else{
-		
+
 		left += arr[i];
 
 		}
@@ -36,14 +36,14 @@ int balancedArrayIndex(vector<int> arr){
 
 int main(){
     vector<int> vec_0;
-    
+
     vec_0.push_back(1); // 0
     vec_0.push_back(200); // 1
     vec_0.push_back(3); // 2
     vec_0.push_back(201); // 3
-    
+
     cout << balancedArrayIndex(vec_0) << "\n";
-    
-    
-    
+
+
+
     }
--- CPP_DS/manage_LinkedList/main.cpp
+++ CPP_DS/manage_LinkedList/main.cpp
@@ -4,7 +4,7 @@
 struct node{
 	int data;
 	struct node* next;
-	
+
 };
 
 class LinkedListMan{
@@ -14,67 +14,67 @@ class LinkedListMan{
 		this->_Head = NULL;
 
 	}
-		
+
 	~LinkedListMan(){
 
 		this->_Head = NULL;
-		
-		
+
+
 	}
-		
-		
-		
+
+
+
 	struct node* findMiddle(){
 
 		int count = 0;
-		struct node *_tempNode = _Head; 
-		struct node *mid = _tempNode; 
-	  
-		while ( _tempNode ) 
-		{ 
+		struct node *_tempNode = _Head;
+		struct node *mid = _tempNode;
+
+		while ( _tempNode )
+		{
 
 			/* update mid, when 'count' is odd number */
-			if (count & 1) 
-				mid = mid->next; 
+			if (count & 1)
+				mid = mid->next;
 
 
-	  
-			++count; 
+
+			++count;
 			if(count>10) break;
-			_tempNode = _tempNode->next; 
-		} 
+			_tempNode = _tempNode->next;
+		}
 
 		return mid;
 
 	}
-	
-	
+
+
 	void printLL(){
 		printf("newLL:");
 		for(struct node *_tempNode= _Head ; _tempNode ; _tempNode = _tempNode->next){
-			
+
 			printf("%d ",_tempNode->data);
 
 		}
 			printf("\n");
-		
-		
+
+
 	}
-	
+
 
 	bool delNode(struct node **_node){
 		struct node * _tempNode= _Head;
-		
+
 		while( _tempNode->next != (*_node) ){
 			_tempNode = _tempNode->next;
-			
+
 		}
 		_tempNode->next = (*_node)->next;
 		free(*_node);
 		return true;
 
 	}
-	
+
 
 	bool addNode(struct node **_node){
 
@@ -85,12 +85,12 @@ class LinkedListMan{
 
 			struct node *_tempNode;
 			_tempNode = _Head;
-			
+
 			while( _tempNode->next ){
 				_tempNode = _tempNode->next;
 			}
 			_tempNode->next = *_node;
-			
+
 		}
 		return true;
 
@@ -102,8 +102,8 @@ class LinkedListMan{
 
 	private:
 	struct node* _Head;
-		
-		
+
+
 };
 
 int main(){
@@ -120,13 +120,13 @@ int main(){
 		LL = new LinkedListMan();//&Head
 		if(LL == NULL ){
 
-			printf("Case %d: ",i); 
+			printf("Case %d: ",i);
 			printf("FAIL: main():new LinkedListMan.\n");
 
 			continue;
-			
+
 		}
-		
+
 		for( int j=0 ; j<size ; ++j){
 
 			//Add node to Linked List.
@@ -134,7 +134,7 @@ int main(){
 			_node = (struct node*)malloc( sizeof( struct node ));
 			if(_node == NULL ){
 				printf("FAILED _node malloc\n");
-				
+
 			}
 
 
@@ -146,13 +146,13 @@ int main(){
 				printf("FAIL: main(): LL.addNode()::data=%d \n",_node->data);
 				continue;
 			}
-			
+
 
 		}
 
 		struct node* _midNode = LL->findMiddle();
-		printf("Case %d: %d \n",i,_midNode->data); 
-		
+		printf("Case %d: %d \n",i,_midNode->data);
+
 		//printf("Deleting Middle.\n");
 		bool sucess = LL->delNode(&_midNode);
 		LL->printLL();
@@ -162,6 +162,6 @@ int main(){
 
 	}
 
-    return 0; 
+    return 0;
 
 }
